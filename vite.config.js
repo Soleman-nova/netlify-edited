77eb646f5f076c68be7f171b61b08dbfa2e70398
Soleman-nova <selamud2@gmail.com>
2025-03-21 17:16:24 +0300

    first netlify edited

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss() ],
+  //publicDir: 'public',
+  //base: process.env.VITE_BASE_PATH || '/Movies_website',
+})
+
+//
+// import { defineConfig } from 'vite'
+//  import react from '@vitejs/plugin-react'
+//  import tailwindcss from '@tailwindcss/vite';
+//
+// // export default defineConfig({
+// //   plugins: [react(), tailwindcss()],
+// //   // base: process.env.VITE_BASE_PATH || '/Movies_website',
+//  })
+//
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// //import tailwindcss from '@tailwindcss/vite';
+//
+//
+//
+// export default defineConfig({
+//   base: '/', // Keep this for Netlify deployment
+//   plugins: [react()],
+//   publicDir: 'public',
+// });
